557fa5d90fcb4b4608014133f8c229714f77859c
Gavin Haseltine <gavinhaseltine@gmail.com>
2023-08-17 17:54:53 -0500

    A buchostuff

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 export function PizzaItem({ index, pizza }) {
+  const dispatch = useDispatch()
+
 
   const[buttonStatus, setButtonStatus] = useState(true)
 
   function addToCart(){
       console.log('adding')
       setButtonStatus(!buttonStatus)
+
+      let pizzaItem = {
+        name: pizza.name,
+       id: pizza.id,
+       price: pizza.price
+
+      }
+
+      dispatch({
+        type: 'GET_CART',
+        payload: pizzaItem
+      })
   }
 
   function removeFromCart(){
     console.log('removing')
     setButtonStatus(!buttonStatus)
+
+    let pizzaItem = {
+      name: pizza.name,
+     id: pizza.id,
+     price: pizza.price
+
+    }
+
+    dispatch({
+      type: 'REMOVE_ITEM_CART',
+      payload: pizzaItem
+    })
   }
 
   return (<><li><b>{pizza.name}:</b> {pizza.description} - {pizza.price}</li>
